fd693799280ed86e29885f71e1dafa091a2b8ab1
Nguyễn Việt Hoàn <95644536+viethoan261@users.noreply.github.com>
2023-05-09 15:48:50 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5174,
-  },
-  base: '/Money-Honey-LVT/ecommerce-customer-fe-2023',
 });
